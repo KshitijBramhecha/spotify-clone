0e311961242cd650a355d8e48fac10fe810c9a87
unknown <kshitijlm10b@gmail.com>
2023-12-17 19:19:33 +0530

    final

---
+++ hooks/useUser.tsx
@@ -0,0 +1,83 @@
+import { Subscription, UserDetails } from "@/types";
+import { User } from "@supabase/auth-helpers-nextjs";
+import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
+import { createContext, useContext, useEffect, useState } from "react";
+
+type UserContextType = {
+    accessToken: string | null;
+    user: User | null;
+    userDetails: UserDetails | null;
+    isLoading: boolean;
+    subscription: Subscription | null;
+}
+
+export const UserContext = createContext<UserContextType | undefined>(
+    undefined
+);
+
+export interface Props {
+    [propName: string]: any;
+}
+
+export const MyUserContextProvider = (props: Props) => {
+    const {
+        session,
+        isLoading: isLoadingUser, 
+        supabaseClient: supabase,
+
+    } = useSessionContext();
+    const user = useSupaUser();
+    const accessToken = session?.access_token ?? null;
+    const [isLoadingData, setIsLoadingData] = useState(false);
+    const [userDetails, setuserDetails] = useState<UserDetails | null>(null);
+    const [subscription, setSupscription] = useState<Subscription | null>(null);
+
+    const getUserDetails = () => supabase.from('users').select('*').single();
+    const getSubscription = () => supabase.from('subscriptions').select('*, prices(*, products(*))').in('status',['trialing','active']);
+
+    useEffect(() => {
+       if(user && !isLoadingData && !userDetails && !subscription){
+        setIsLoadingData(true);
+
+        Promise.allSettled([getUserDetails(),getSubscription()]).then(
+            (results) => {
+                const userDetailsPromise = results[0];
+                const subscriptionPromise = results[0];
+
+                if(userDetailsPromise.status === "fulfilled") {
+                    setuserDetails(userDetailsPromise.value.data as UserDetails);
+                }
+
+                if(subscriptionPromise.status === "fulfilled") {
+                    setSupscription(subscriptionPromise.value.data as Subscription);
+                }
+
+                setIsLoadingData(false);
+            }
+        )
+       }
+       else if(!user && !isLoadingUser && !isLoadingData ){
+        setuserDetails(null);
+        setSupscription(null);
+       }
+    }, [user, isLoadingUser]);
+
+    const value = {
+        accessToken,
+        user,
+        userDetails,
+        isLoading: isLoadingUser || isLoadingData,
+        subscription
+    }
+
+    return<UserContext.Provider value={value} {...props} />
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if(context === undefined){
+        throw new Error('useUser must be used within a MyUserContextProvider')
+    }
+
+    return context;
+}
---
+++ types/use-sound.d.ts
@@ -0,0 +1,5 @@
+declare module 'use-sound' {
+    export default function useSound(url: string, options?: any): [() => void, any];
+    // Add more type declarations as needed based on the library's API
+  }
+  
